a985b54856d6cd5cd23c04c5aaf83261c944db3a
Marchael Acode <m.acode@outlook.com>
2020-10-12 13:47:15 -0700

    Added book and error controller, added book class and repository, added fragements head and header, added book and error page.

---
+++ src/main/java/teamEarth/homeSchoolHelper/controllers/BookController.java
@@ -0,0 +1,23 @@
+package teamEarth.homeSchoolHelper.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+import teamEarth.homeSchoolHelper.models.book.BookRepository;
+
+@Controller
+public class BookController {
+
+    // TODO: @Autowired to other object repositories
+//    @Autowired
+//    @Autowired
+//    BookRepository bookRepository;
+//
+//    @PostMapping("/book")
+//    public RedirectView addNewBook(String subject, String title, String author, String ISBN,
+//                                   String edition, String gradeLevel, int numberOfUnits) throws Exception {
+//        System.out.println("Adding a new book!");
+//    }
+
+}
---
+++ src/main/java/teamEarth/homeSchoolHelper/models/book/BookRepository.java
@@ -0,0 +1,6 @@
+package teamEarth.homeSchoolHelper.models.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
