51e68b72b341b70bfb6cd30b7dbb494cb78bd171
Marchael Acode <m.acode@outlook.com>
2020-10-12 15:27:50 -0700

    Added a myprofile page

--- src/main/java/teamEarth/homeSchoolHelper/models/book/Book.java
+++ src/main/java/teamEarth/homeSchoolHelper/models/book/Book.java
@@ -1,10 +1,8 @@
 package teamEarth.homeSchoolHelper.models.book;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
+@Entity
 public class Book {
 
     @Id
