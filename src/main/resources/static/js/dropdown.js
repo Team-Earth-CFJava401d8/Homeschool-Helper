40c01a4f9e251a8b0d4abde5d06871810178e924
Paul OBrien <PVOVideo@outlook.com>
2020-10-13 15:18:49 -0700

    add dropdown menu for student and lessonplan

---
+++ src/main/resources/static/js/dropdown.js
@@ -0,0 +1,23 @@
+"use strict";
+
+  // Where this script came from: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown_right
+
+function dropDown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+        } else {
+        a[i].style.display = "none";
+        }
+    }
+}
